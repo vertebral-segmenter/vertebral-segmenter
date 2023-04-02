ede4e58ee2078a3fff092acbdc512582a0e2d08c
S.Mohammadsaleh M.Tabatabaei <seyedsaleh.edu@gmail.com>
2023-04-02 17:06:26 -0400

    Extracting serie-2000 segmentations

--- data_preprocessing/data_extracting_Osteosarcopenia.py
+++ data_preprocessing/data_extracting_Osteosarcopenia.py
@@ -11,13 +11,23 @@ for root, dirs, files in os.walk(src_path):
     if 'Stereology' in root and files:
         for file in files:
             if file.lower().endswith('nii'):
+                src_file = os.path.join(root, file)
                 if 'scan' in file.lower() and 'resampled' in file.lower():
-                    src_file = os.path.join(root, file)
-                    shutil.copy(src_file, dst_path)
-
                     scan_number = [num for num in src_file.split('\\') if num.isnumeric()][0]
                     dst_file_name = f"{scan_number}_scan_cropped.nii"
-                    dst_file = os.path.join(dst_path, dst_file_name)
-                    os.rename(os.path.join(dst_path, file), dst_file)
-
-                    print(f"{dst_file_name} data copied...")
+                    if dst_file_name in os.listdir(dst_path):
+                        print(f"# {dst_file_name} already exist in the dataset.")
+                        continue
+                    shutil.copy(src_file, dst_path)
+                elif 'seg' in file.lower() and 'resampled' in file.lower():
+                    scan_number = [num for num in src_file.split('\\') if num.isnumeric()][0]
+                    dst_file_name = f"{scan_number}_seg.nii"
+                    if dst_file_name in os.listdir(dst_path):
+                        print(f"# {dst_file_name} already exist in the dataset.")
+                        continue
+                    shutil.copy(src_file, dst_path)
+                else:
+                    continue
+                dst_file = os.path.join(dst_path, dst_file_name)
+                os.rename(os.path.join(dst_path, file), dst_file)
+                print(f"{dst_file_name} data copied...")
