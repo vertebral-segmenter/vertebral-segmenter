0486ccfe8bdd6ae7f45b308fe094867daf18ffe4
S.Mohammadsaleh M.Tabatabaei <seyedsaleh.edu@gmail.com>
2023-04-14 02:06:17 -0400

    refactor: fix bugs in processing series-800 and 1000 and 1100

--- data_preprocessing/data_extracting_CIHR.py
+++ data_preprocessing/data_extracting_CIHR.py
@@ -3,7 +3,8 @@ import os
 import shutil
 import nibabel as nib
 from data_preprocessing.image_analysis.amira_processing import convert_amira_to_nifti
-from data_preprocessing.image_analysis.nifti_processing import process_segmentation_image, process_scan_image, load_as_nifti
+from data_preprocessing.image_analysis.nifti_processing import process_segmentation_image, process_scan_image, \
+    load_as_nifti
 import logging
 
 
@@ -22,7 +23,8 @@ def setup_logger(name, log_file, level=logging.DEBUG, print_log=True):
         stream_handler.setLevel(logging.DEBUG)
 
     # Create a formatter and add it to the file handler
-    file_handler_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+    file_handler_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s',
+                                               datefmt='%m/%d/%Y %I:%M:%S %p')
     file_handler.setFormatter(file_handler_formatter)
     if print_log:
         stream_handler_formatter = logging.Formatter('%(levelname)s - %(message)s')
@@ -71,9 +73,11 @@ def convert_and_copy_image(src_file, dst_path, new_file_name=None, new_file_exte
     if file_extension == '.am' and new_file_extension == '.nii':
         try:
             nifti_img = input_img
-            logger.info(f">Amira to Nifti converted: {src_file}") if logger else print(f">Amira to Nifti converted: {src_file}")
+            logger.info(f">Amira to Nifti converted: {src_file}") if logger else print(
+                f">Amira to Nifti converted: {src_file}")
         except:
-            logger.error(f"Error in converting Amira file: {src_file}") if logger else print(f"Error in converting Amira file: {src_file}")
+            logger.error(f"Error in converting Amira file: {src_file}") if logger else print(
+                f"Error in converting Amira file: {src_file}")
             return
     else:
         nifti_img = input_img
@@ -85,12 +89,14 @@ def convert_and_copy_image(src_file, dst_path, new_file_name=None, new_file_exte
         else:
             processed_img = process_scan_image(nifti_img)
     except:
-        logger.error(f"Error in processing file: {src_file}.") if logger else print(f"Error in processing file: {src_file}.")
+        logger.error(f"Error in processing file: {src_file}.") if logger else print(
+            f"Error in processing file: {src_file}.")
         return
 
     # Save the processed image in the destination path
     nib.save(processed_img, os.path.join(dst_path, file))
-    logger.info(f"{'Segmentation' if is_label else 'Scan'} Copied: {src_file} -> {file}") if logger else print(f"{'Segmentation' if is_label else 'Scan'} Copied: {src_file} -> {file}")
+    logger.info(f"{'Segmentation' if is_label else 'Scan'} Copied: {src_file} -> {file}") if logger else print(
+        f"{'Segmentation' if is_label else 'Scan'} Copied: {src_file} -> {file}")
 
 
 def scan_number(root_path):
@@ -105,25 +111,25 @@ def scan_number(root_path):
 
 
 # iterate over amira files in the source directory (series 700)
-# def process_700_series(src_path, dst_scan_path=r"D:\Rat_mCT_v1\scans", dst_label_path=r"D:\Rat_mCT_v1\labels",
-#                        logger):
-#     series700_path = os.path.join(src_path, "700-Series")
-#     for root, dirs, files in os.walk(series700_path):
-#         if 'L2' in root and files:
-#             for file in files:
-#                 if os.path.splitext(file.lower())[1] == '.am':
-#                     if (
-#                             'l1-l3' in file.lower() or 'l2' in file.lower()) and not 'upsampled' in file.lower() and not 'mask' in file.lower() and not 'vbcort' in file.lower():
-#                         src_file = os.path.join(root, file)
-#                         dst_file_name = f"{scan_number(src_file)}_scan_cropped.nii"
-#                         dst_file = os.path.join(dst_scan_path, dst_file_name)
-#                         try:
-#                             nifti_img = convert_amira_to_nifti(src_file)
-#                             # Save the NIfTI image
-#                             nib.save(nifti_img, dst_file)
-#                             print(f"{dst_file_name} created...")
-#                         except:
-#                             print(f"## Error - Can't convert {file}.")
+def process_700_series(src_path, dst_scan_path=r"D:\Rat_mCT_v1\scans", dst_label_path=r"D:\Rat_mCT_v1\labels",
+                       logger=None):
+    series700_path = os.path.join(src_path, "700-Series")
+    for root, dirs, files in os.walk(series700_path):
+        if 'L2' in root and files:
+            for file in files:
+                if os.path.splitext(file.lower())[1] == '.am':
+                    if (
+                            'l1-l3' in file.lower() or 'l2' in file.lower()) and not 'upsampled' in file.lower() and not 'mask' in file.lower() and not 'vbcort' in file.lower():
+                        src_file = os.path.join(root, file)
+                        dst_file_name = f"{scan_number(src_file)}_scan_cropped.nii"
+                        dst_file = os.path.join(dst_scan_path, dst_file_name)
+                        try:
+                            nifti_img = convert_amira_to_nifti(src_file)
+                            # Save the NIfTI image
+                            nib.save(nifti_img, dst_file)
+                            print(f"{dst_file_name} created...")
+                        except:
+                            print(f"## Error - Can't convert {file}.")
 
 
 # iterate over files in the source directory (series 800)
@@ -133,21 +139,20 @@ def process_800_series(src_path, dst_scan_path=r"D:\Rat_mCT_v1\scans", dst_label
     for root, dirs, files in os.walk(series800_path):
         if 'L2' in root and files:
             for file in files:
-                if file.lower().endswith('nii'):
+                if file.lower().endswith('nii') and file.lower().endswith('nrrd'):
                     if 'resampled' in file.lower() or 'scan' in file.lower():
                         src_file = os.path.join(root, file)
                         if '_segmentation' in file.lower():
                             is_label = True
                             file_name = f"{scan_number(src_file)}_segmentation"
-                            convert_and_copy_image(src_file, dst_label_path, new_file_name=file_name, new_file_extension='.nii',
+                            convert_and_copy_image(src_file, dst_label_path, new_file_name=file_name,
+                                                   new_file_extension='.nii',
                                                    is_label=is_label, logger=logger)
                         elif 'segmentation' not in file.lower():
                             is_label = False
                             file_name = f"{scan_number(src_file)}_scan_cropped"
                             convert_and_copy_image(src_file, dst_scan_path, new_file_name=file_name,
                                                    is_label=is_label, logger=logger)
-
-
 
 
 # iterate over files in the source directory (series 900)
@@ -157,7 +162,7 @@ def process_900_series(src_path, dst_scan_path=r"D:\Rat_mCT_v1\scans", dst_label
     for root, dirs, files in os.walk(series900_path):
         if 'L2' in root and files:
             for file in files:
-                if file.lower().endswith('nii'):
+                if file.lower().endswith('nii') and file.lower().endswith('nrrd'):
                     if 'resampled' in file.lower():
                         src_file = os.path.join(root, file)
                         is_label = True if 'segmentation' in file.lower() else False
@@ -172,7 +177,7 @@ def process_1000_series(src_path, dst_scan_path=r"D:\Rat_mCT_v1\scans", dst_labe
                         logger=None):
     series1000_path = os.path.join(src_path, "1000-Series")
     for root, dirs, files in os.walk(series1000_path):
-        if 'L2' in root and files:
+        if 'L2' in root and files and 'old' not in root:
             for file in files:
                 src_file = os.path.join(root, file)
                 if file.lower().endswith('nii'):
@@ -202,14 +207,18 @@ def process_1000_series(src_path, dst_scan_path=r"D:\Rat_mCT_v1\scans", dst_labe
 
 
 # iterate over files in the source directory (series 1100)
-def process_1100_series(src_path, dst_scan_path=r"D:\Rat_mCT_v1\scans", dst_label_path=r"D:\Rat_mCT_v1\labels", logger=None):
+def process_1100_series(src_path, dst_scan_path=r"D:\Rat_mCT_v1\scans", dst_label_path=r"D:\Rat_mCT_v1\labels",
+                        logger=None):
     series1100_path = os.path.join(src_path, "1100-Series")
     for root, dirs, files in os.walk(series1100_path):
         if 'L2' in root and files:
             for file in files:
                 src_file = os.path.join(root, file)
-                if file.lower().endswith('nii') and 'CIHR' in file and (
-                        'L4_cropped_resampled' in file or 'L2_resampled' in file):
+                if int(scan_number(src_file)) in [1104, 1105, 1106]:
+                    # should extract data manually for CT 1104, 1105, 1106
+                    continue
+                elif file.lower().endswith('nii') and 'CIHR' in file and (
+                        'L4_cropped_resampled' in file or 'L4_resampled_cropped' in file or 'L2_resampled' in file):
                     file_name = f"{scan_number(src_file)}_scan_cropped"
                     convert_and_copy_image(src_file, dst_scan_path, new_file_name=file_name, new_file_extension='.nii',
                                            is_label=False, logger=logger)
@@ -217,21 +226,17 @@ def process_1100_series(src_path, dst_scan_path=r"D:\Rat_mCT_v1\scans", dst_labe
                     file_name = f"{scan_number(src_file)}_segmentation"
                     convert_and_copy_image(src_file, dst_label_path, new_file_name=file_name, new_file_extension='.nii',
                                            is_label=True, logger=logger)
-                elif scan_number(src_file) in range(1107, 1111) and file.endswith('L2.nii'):
+                elif int(scan_number(src_file)) in range(1107, 1111) and file.endswith('L2.nii'):
                     file_name = f"{scan_number(src_file)}_scan_cropped"
                     convert_and_copy_image(src_file, dst_scan_path, new_file_name=file_name, new_file_extension='.nii',
                                            is_label=False, logger=logger)
-                elif scan_number(src_file) in range(1107, 1111) and file.endswith('L2_SlicerSegmentation.nii'):
+                elif int(scan_number(src_file)) in range(1107, 1111) and file.endswith('L2_SlicerSegmentation.nii'):
                     file_name = f"{scan_number(src_file)}_segmentation"
                     convert_and_copy_image(src_file, dst_label_path, new_file_name=file_name, new_file_extension='.nii',
                                            is_label=True, logger=logger)
-                elif scan_number(src_file) in range(1104, 1117):
-                    # should extract data manually
-                    pass
 
 
-if __name__ == "__main__":
-
+def main():
     # assign directories
     src_path = r"T:\CIHR Data\16) Stereology"
     dst_scan_path = r"D:\Rat_mCT_v1\scans"
@@ -249,6 +254,10 @@ if __name__ == "__main__":
     process_900_series(src_path, dst_scan_path, dst_label_path, logger=process_900_logger)
     process_1000_series(src_path, dst_scan_path, dst_label_path, logger=process_1000_logger)
     process_1100_series(src_path, dst_scan_path, dst_label_path, logger=process_1100_logger)
+
+
+if __name__ == "__main__":
+    main()
 
 # # Rename manually copied files in destination folder
 # for root, dirs, files in os.walk(dst_path):
