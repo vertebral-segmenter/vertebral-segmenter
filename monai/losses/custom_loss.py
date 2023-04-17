00cd928da987554b41833bc8fd345ebf95cc1afb
Alexander Ryabchenko <alexander.ryabchenko@mail.utoronto.ca>
2023-04-16 22:19:43 -0400

    updated loss parameters

--- monai/losses/custom_loss.py
+++ monai/losses/custom_loss.py
@@ -7,7 +7,7 @@ from monai.networks import one_hot
 class CustomLoss(nn.Module):
     """Weighted Asymmetric Unified Focal loss and MSE on Volume ratios
     """
-    def __init__(self, to_onehot_y=False, weightAUF=0.75, weightBVTV=1., weight=0.5, delta=0.6, gamma=0.2, epsilon=1e-07):
+    def __init__(self, to_onehot_y=False, weightAUF=2., weightBVTV=1., weight=0.5, delta=0.6, gamma=0.8, epsilon=1e-07):
         super(CustomLoss, self).__init__()
         self.weightAUF = weightAUF      # weights
         self.weightBVTV = weightBVTV    # weights
@@ -21,7 +21,7 @@ class CustomLoss(nn.Module):
         # Obtain Asymmetric Unified Focal loss
         if self.to_onehot_y:
             y_true = one_hot(y_true, num_classes=y_pred.shape[1])
-        auf_loss = AsymmetricUnifiedFocalLoss(delta=self.delta, gamma=self.gamma)(y_pred, y_true)
+        auf_loss = AsymmetricUnifiedFocalLoss(weight=self.weight, delta=self.delta, gamma=self.gamma)(y_pred, y_true)
         # Obtain BVTV ration mse
         vols_pred = torch.sum(y_pred, dim=[2, 3, 4])
         bvtv_pred = (vols_pred[:, 1]) / (vols_pred[:, 0] + vols_pred[:, 1])
@@ -34,7 +34,17 @@ class CustomLoss(nn.Module):
 
 
 if __name__ == "__main__":
-    shapes = (100, 2, 10, 10, 10)
-    y_true = torch.randint(low=0, high=2, size=shapes)
-    y_pred = 1 - y_true # torch.rand(shapes)
-    print(CustomLoss()(y_true, y_pred))
+    import matplotlib.pyplot as plt
+    x_lst = []
+    y_lst = []
+    for i in range(0, 96**3, 1000):
+        tr = int(96**3 / 2)
+        y_true = torch.cat((torch.ones(tr), torch.zeros(96**3-tr))).reshape(1, 1, 96, 96, 96)
+        fr = i
+        y_pred = torch.cat((torch.ones(fr), torch.zeros(96**3-fr))).reshape(1, 1, 96, 96, 96)
+        y_pred = one_hot(y_pred, num_classes=2)
+        x_lst.append(i)
+        y_lst.append(CustomLoss(to_onehot_y=True)(y_pred, y_true))
+    plt.plot(x_lst, y_lst)
+    plt.show()
+
